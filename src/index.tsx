71e4aac375136c02512ff53ca97b5d733a8018ff
Forrest Wolf <forrest.weiswolf@gmail.com>
2025-04-17 13:32:56 -0400

    Show probs after latest bet

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,46 @@
 import { render } from 'preact';
 import { useState } from 'preact/hooks';
 import './style.css';
-import { fetchMarket, fetchBets } from './api';
-import { Bet, Market } from './types';
+import { fetchMarket, fetchBets, fetchUser } from './api';
+import { Bet, Market, User } from './types';
+
+const getLastBetProb = (bets: Bet[], market: Market, userId: string) => {
+	const bet = bets.sort((a, b) => b.createdTime - a.createdTime).find((b) => b.contractId === market.id && b.userId === userId)
+	console.log({ bets, market, userId, bet })
+	if (!bet) { return null }
+
+	return bet.probAfter
+}
+
+const formatProb = (prob: number) => `${Math.round(prob * 100)}%`
 
 export function App() {
 	const [usernames, setUsernames] = useState(['', ''] as [string, string])
+	const [userIds, setUserIds] = useState<[string?, string?]>([null, null])
+
 	const [commonMarkets, setCommonMarkets] = useState<Market[]>([])
+	const [commonBets, setCommonBets] = useState<Bet[]>([])
 
 	const fetchCommonMarkets = async ([user1, user2]: [string, string]) => {
 		const bets = await Promise.all([fetchBets({ username: usernames[0] }), fetchBets({ username: usernames[1] })])
+		setUserIds([bets[0][0].userId, bets[1][0].userId])
 		const commonMarketIds = new Set<string>()
 		bets[0].forEach((bet) => {
 			if (bets[1].some((b) => b.contractId === bet.contractId)) { commonMarketIds.add(bet.contractId) }
 		})
 
-		const commonMarkets = await Promise.all(
+		const commonBets = bets[0].sort((a, b) => a.createdTime - b.createdTime).filter((bet) => commonMarketIds.has(bet.contractId))
+
+		const commonMarkets = Promise.all(
 			Array.from(commonMarketIds).map((id) =>
 				fetchMarket(id)
 			)
 		)
 
-		setCommonMarkets(commonMarkets.filter(m => !m.isResolved).sort((a, b) => a.closeTime - b.closeTime))
-		console.log(commonMarketIds, commonMarkets)
+		setCommonBets(commonBets)
+		setCommonMarkets((await commonMarkets).filter(
+			m => !m.isResolved
+		).sort((a, b) => a.closeTime - b.closeTime))
 	}
 
 	return (
@@ -43,7 +61,12 @@ export function App() {
 			<div>
 				{commonMarkets.map((market) => (
 					<div key={market.id}>
-						<a href={market.url}>{market.question}{market.probability ? `: (${Math.round(market.probability * 100)}%)` : ''}</a>
+						<a href={market.url}>{market.question}{market.probability ? `: ${formatProb(market.probability)}%` : ''}</a>
+						<div>
+							<span>{usernames[0]}: {getLastBetProb(commonBets, market, userIds[0]) ? formatProb(getLastBetProb(commonBets, market, userIds[0])) : 'N/A'}</span>
+							<br />
+							<span>{usernames[1]}: {getLastBetProb(commonBets, market, userIds[1]) ? formatProb(getLastBetProb(commonBets, market, userIds[1])) : 'N/A'}</span>
+						</div>
 					</div>
 				))}
 			</div>
