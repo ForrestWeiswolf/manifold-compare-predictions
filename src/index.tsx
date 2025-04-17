273583615db02b6642de6fffda0b6c86fbf439f7
Forrest Wolf <forrest.weiswolf@gmail.com>
2025-04-17 12:28:50 -0400

    Fetch on 'enter' as well as on button click

--- src/index.tsx
+++ src/index.tsx
@@ -10,21 +10,27 @@ export function App() {
 			console.log(markets)
 		})
 	}, [])
+
+	const fetchUsers = async () => {
+		const users = await Promise.all([fetchUser(usernames[0]), fetchUser(usernames[1])])
+		console.log(users)
+	}
+
 	return (
 		<div>
 			<input type="text" name="username1" value={usernames[0]}
 				onChange={(e) => setUsernames([(e.target as HTMLInputElement).value, usernames[1]])}
+				onKeyDown={(e) => {
+					if (e.key === 'Enter') { fetchUsers() }
+				}}
 			/>
 			<input type="text" name="username2" value={usernames[1]}
 				onChange={(e) => setUsernames([usernames[0], (e.target as HTMLInputElement).value])}
+				onKeyDown={(e) => {
+					if (e.key === 'Enter') { fetchUsers() }
+				}}
 			/>
-			<button onClick={async () => {
-				const username1 = usernames[0]
-				const username2 = usernames[1]
-				console.log(username1, username2)
-				const users = await Promise.all([fetchUser(username1), fetchUser(username2)])
-				console.log(users)
-			}}>Fetch</button>
+			<button onClick={fetchUsers}>Fetch</button>
 		</div>
 	);
 }
