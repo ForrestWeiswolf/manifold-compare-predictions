f637207425b32b8e7d5913ac1ce93c49136ab7fe
Forrest Wolf <forrest.weiswolf@gmail.com>
2025-04-17 12:27:02 -0400

    Fetch data of two entered usernames

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,31 @@
 import { render } from 'preact';
-import { useEffect } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 import './style.css';
-import { fetchMarkets } from './api';
+import { fetchMarkets, fetchUser } from './api';
 
 export function App() {
+	const [usernames, setUsernames] = useState(['', ''])
 	useEffect(() => {
-		fetchMarkets({ }).then((markets) => {
+		fetchMarkets({}).then((markets) => {
 			console.log(markets)
 		})
 	}, [])
 	return (
 		<div>
-			<h1>Hello World</h1>
+			<input type="text" name="username1" value={usernames[0]}
+				onChange={(e) => setUsernames([(e.target as HTMLInputElement).value, usernames[1]])}
+			/>
+			<input type="text" name="username2" value={usernames[1]}
+				onChange={(e) => setUsernames([usernames[0], (e.target as HTMLInputElement).value])}
+			/>
+			<button onClick={async () => {
+				const username1 = usernames[0]
+				const username2 = usernames[1]
+				console.log(username1, username2)
+				const users = await Promise.all([fetchUser(username1), fetchUser(username2)])
+				console.log(users)
+			}}>Fetch</button>
 		</div>
-	);
-}
-
-function Resource(props) {
-	return (
-		<a href={props.href} target="_blank" class="resource">
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-		</a>
 	);
 }
 
