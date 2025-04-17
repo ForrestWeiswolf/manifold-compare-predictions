4e0d4ea6647fadf4950a36ea52aeedc3da690108
Forrest Wolf <forrest.weiswolf@gmail.com>
2025-04-17 15:23:40 -0400

    Only show binary markets

--- src/index.tsx
+++ src/index.tsx
@@ -38,7 +38,7 @@ export function App() {
 
 		setCommonMarkets(
 			(await commonMarkets)
-				.filter(m => !m.isResolved)
+				.filter(m => !m.isResolved && m.outcomeType === 'BINARY')
 				.map(m => ({ ...m, userProbs: userIds.map(id => getLastBetProb(commonBets, m, id)) }))
 				.sort((a, b) => (a.userProbs[0] - a.userProbs[1]) - (b.userProbs[0] - b.userProbs[1]))
 		);
