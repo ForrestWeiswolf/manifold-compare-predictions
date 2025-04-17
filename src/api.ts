162b80d7789bc6db9a1a29850d202967f9a0253c
Forrest Wolf <forrest.weiswolf@gmail.com>
2025-04-17 15:13:40 -0400

    Set up and run linter

--- src/api.ts
+++ src/api.ts
@@ -1,31 +1,33 @@
-import { Market, User, Bet } from './types'
+import { Market, User, Bet } from './types';
 
-const API_URL = 'https://api.manifold.markets/v0'
+const API_URL = 'https://api.manifold.markets/v0';
 
-type GetBetsParams = Partial<{ limit: number, before: string, after: string, order: string, userId: string, username: string, contractId: string, contractSlug: string }>
+type GetBetsParams = Partial<{
+  limit: number, before: string, after: string, order: string, userId: string, username: string, contractId: string, contractSlug: string
+}>;
 
-const DEFAULT_BETS_PARAMS: GetBetsParams = {}
+const DEFAULT_BETS_PARAMS: GetBetsParams = {};
 
 const paramsToString = (params: Record<string, string | number | undefined>) => {
   return new URLSearchParams(
     Object.fromEntries(
       Object.entries(params).map(([key, value]) => [key, value.toString()])
     )
-  ).toString()
-}
+  ).toString();
+};
 
 export async function fetchMarket(id: string) {
-  const response = await fetch(`${API_URL}/market/${id}`)
-  return response.json() as Promise<Market>
+  const response = await fetch(`${API_URL}/market/${id}`);
+  return response.json() as Promise<Market>;
 }
 
 export async function fetchUser(username: string) {
-  const response = await fetch(`${API_URL}/user/${username}`)
-  return response.json() as Promise<User>
+  const response = await fetch(`${API_URL}/user/${username}`);
+  return response.json() as Promise<User>;
 }
 
 export async function fetchBets(params: GetBetsParams) {
-  const queryParams = paramsToString({ ...DEFAULT_BETS_PARAMS, ...params })
-  const response = await fetch(`${API_URL}/bets?${queryParams}`)
-  return response.json() as Promise<Bet[]>
+  const queryParams = paramsToString({ ...DEFAULT_BETS_PARAMS, ...params });
+  const response = await fetch(`${API_URL}/bets?${queryParams}`);
+  return response.json() as Promise<Bet[]>;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,65 +1,65 @@
 import { render } from 'preact';
 import { useState } from 'preact/hooks';
 import './style.css';
-import { fetchMarket, fetchBets, fetchUser } from './api';
+import { fetchMarket, fetchBets } from './api';
 import { Bet, Market } from './types';
 
 const getLastBetProb = (bets: Bet[], market: Market, userId: string) => {
-	const bet = bets.sort((a, b) => b.createdTime - a.createdTime).find((b) => b.contractId === market.id && b.userId === userId)
-	console.log({ bets, market, userId, bet })
-	if (!bet) { return null }
+	const bet = bets.sort((a, b) => b.createdTime - a.createdTime).find((b) => b.contractId === market.id && b.userId === userId);
+	console.log({ bets, market, userId, bet });
+	if (!bet) { return null; }
 
-	return bet.probAfter
-}
+	return bet.probAfter;
+};
 
-const formatProb = (prob: number) => `${Math.round(prob * 100)}%`
+const formatProb = (prob: number) => `${Math.round(prob * 100)}%`;
 
 export function App() {
-	const [usernames, setUsernames] = useState(['', ''] as [string, string])
-	const [commonMarkets, setCommonMarkets] = useState<Array<Market & { userProbs: number[] }>>([])
+	const [usernames, setUsernames] = useState(['', ''] as [string, string]);
+	const [commonMarkets, setCommonMarkets] = useState<Array<Market & { userProbs: number[] }>>([]);
 
-	const fetchCommonMarkets = async ([user1, user2]: [string, string]) => {
-		const bets = await Promise.all([fetchBets({ username: usernames[0] }), fetchBets({ username: usernames[1] })])
-		const userIds = [bets[0][0].userId, bets[1][0].userId]
-		const commonMarketIds = new Set<string>()
+	const fetchCommonMarkets = async () => {
+		const bets = await Promise.all([fetchBets({ username: usernames[0] }), fetchBets({ username: usernames[1] })]);
+		const userIds = [bets[0][0].userId, bets[1][0].userId];
+		const commonMarketIds = new Set<string>();
 		bets[0].forEach((bet) => {
-			if (bets[1].some((b) => b.contractId === bet.contractId)) { commonMarketIds.add(bet.contractId) }
-		})
+			if (bets[1].some((b) => b.contractId === bet.contractId)) { commonMarketIds.add(bet.contractId); }
+		});
 
 		const commonBets = [
 			...bets[0].sort((a, b) => a.createdTime - b.createdTime).filter((bet) => commonMarketIds.has(bet.contractId)),
 			...bets[1].sort((a, b) => a.createdTime - b.createdTime).filter((bet) => commonMarketIds.has(bet.contractId))
-		]
+		];
 
 		const commonMarkets = Promise.all(
 			Array.from(commonMarketIds).map((id) =>
 				fetchMarket(id)
 			)
-		)
+		);
 
 		setCommonMarkets(
 			(await commonMarkets)
 				.filter(m => !m.isResolved)
 				.map(m => ({ ...m, userProbs: userIds.map(id => getLastBetProb(commonBets, m, id)) }))
 				.sort((a, b) => (a.userProbs[0] - a.userProbs[1]) - (b.userProbs[0] - b.userProbs[1]))
-		)
-	}
+		);
+	};
 
 	return (
 		<div>
 			<input type="text" name="username1" value={usernames[0]}
 				onChange={(e) => setUsernames([(e.target as HTMLInputElement).value, usernames[1]])}
 				onKeyDown={(e) => {
-					if (e.key === 'Enter') { fetchCommonMarkets(usernames) }
+					if (e.key === 'Enter') { fetchCommonMarkets(); }
 				}}
 			/>
 			<input type="text" name="username2" value={usernames[1]}
 				onChange={(e) => setUsernames([usernames[0], (e.target as HTMLInputElement).value])}
 				onKeyUp={(e) => {
-					if (e.key === 'Enter') { fetchCommonMarkets(usernames) }
+					if (e.key === 'Enter') { fetchCommonMarkets(); }
 				}}
 			/>
-			<button onClick={() => fetchCommonMarkets(usernames)}>Fetch</button>
+			<button onClick={() => fetchCommonMarkets()}>Fetch</button>
 			<div>
 				{commonMarkets.map((market) => (
 					<div key={market.id}>
