acce6953f5b99143712ce86113fe1bef1886484e
Forrest Wolf <forrest.weiswolf@gmail.com>
2025-04-18 15:45:59 -0400

    Fix type signature of memoize helper

--- src/api.ts
+++ src/api.ts
@@ -2,14 +2,14 @@ import { Market, Bet } from './types';
 
 const API_URL = 'https://api.manifold.markets/v0';
 
-const memoize = <T extends (...args: unknown[]) => Promise<unknown>>(fn: T) => {
-  const calls = new Map<string, Promise<unknown>>();
+const memoize = <T extends (...args: unknown[]) => ReturnType<T>>(fn: T) => {
+  const calls = new Map<string, ReturnType<T>>();
   return async (...args: Parameters<T>) => {
     const key = JSON.stringify(args);
     if (!calls.has(key)) {
       calls.set(key, fn(...args));
     }
-    return calls.get(key);
+    return calls.get(key) as ReturnType<T>;
   };
 };
 
